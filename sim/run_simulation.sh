e30a02290a39f501e76f948b60fed940270db1c8
devadutt-github <72345549+devadutt-github@users.noreply.github.com>
2025-02-19 18:58:27 -0500

    Update run_simulation.sh

--- sim/run_simulation.sh
+++ sim/run_simulation.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Compile the Verilog code
-iverilog -o cache_sim -I ../rtl ../rtl/l1_cache.v ../rtl/l2_cache.v ../rtl/cache_controller.v ../testbench/cache_testbench.v
+# Compile and run the simulation using the Makefile
+make sim
 
-# Run the simulation
-./cache_sim -o waveforms.vcd
-
-# View the waveforms (optional - you can comment this out if you just want to generate the VCD)
-gtkwave waveforms.vcd
+# View GTKWave (optional - you might want to comment this out)
+# gtkwave waveforms.vcd
